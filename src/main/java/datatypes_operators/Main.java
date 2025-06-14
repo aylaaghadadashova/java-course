958813aecd58c9ae833128c9a9490e50d60de401
Ayla <ayla.aghadadashova.std@bhos.edu.az>
2025-06-14 16:29:21 +0400

    data types are done

---
+++ src/main/java/datatypes_operators/Main.java
@@ -0,0 +1,17 @@
+package datatypes_operators;
+
+public class Main {
+    public static void main(String args[]){
+        boolean isTrue = false;
+        byte myByte = -127;
+        char myChar = 'T';
+        short myShort = 56;
+        int myInt = -242341524;
+        long myLong = 7653579373881979862L;
+        float myFloat = 90.78876F;
+        double myDouble = 23.783327428749D;
+
+        System.out.printf("My data types:\nboolean: %b \nbyte: %d \nchar: %c \nshort: %d \nint: %d \nlong: %d \nfloat: %f \ndouble: %.12f", isTrue, myByte, myChar, myShort, myInt, myLong, myFloat, myDouble );
+
+    }
+}
